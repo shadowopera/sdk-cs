fa33ba54b2350bc285c462a1853537783c908a38
edwingeng <edwingeng@outlook.com>
2026-03-06 12:05:32 +0800

    add generated json and code files

---
+++ internal/Conf/AtlasExtension.cs
@@ -0,0 +1,14 @@
+// This file will not be regenerated. Safe to edit.
+
+namespace Conf
+{
+    public class AtlasExtension
+    {
+        // Put your config extensions here.
+
+        public void OnLoaded(ConfigAtlas atlas)
+        {
+            // Initialize your config extensions here.
+        }
+    }
+}
